8960382db701f0c2f29e699f9a824d21cdc18423
Marco2002 <marcoclone@gmail.com>
2023-12-14 00:55:57 +0100

    setup visual chessboard

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <Chessboard :width="8" :height="8" />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Chessboard from '@/components/Chessboard.vue'
+</script>
---
+++ src/components/Chessboard.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="c-chessboard" :style="{
+        width: width*100 +'px',
+        height: height*100+'px'
+    }">
+        <div v-for="i in height" v-bind:key="i" class="c-chessboard__row">
+            <div v-for="j in width" v-bind:key="j" 
+                class="c-chessboard__tile"
+                :class="{
+                    offset: (i+j) % 2 == 1,
+                }">
+                <p v-if="j==1" class="rank">{{i}}</p>
+                <p v-if="i==8" class="file">{{String.fromCharCode(96 + (9-j))}}</p>
+            </div>  
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Chessboard',
+    props: {
+        width: Number,
+        height: Number,
+    }
+}
+</script>
+
+<style>
+
+.c-chessboard {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.c-chessboard__row {
+    display: flex;
+    flex: 1 0;
+    width: 100%;
+}
+
+.c-chessboard__tile {
+    flex: 1 0;
+    background-color: #e9edcc;
+    position: relative;
+}
+
+.c-chessboard__tile.offset {
+    background-color: #779954;
+}
+
+.rank, .file {
+    color: #779954;
+    font-size: 20px;
+}
+
+.offset .rank, .offset .file {
+    color: #e9edcc;
+}
+
+.rank {
+    position: absolute;
+    top: 0;
+    left: 4px;
+}
+.file {
+    position: absolute;
+    bottom: 0;
+    right: 8px;
+}
+</style>

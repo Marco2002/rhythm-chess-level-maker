399193f11ea8c928d0e5b1a09915fdb71780ea2e
Marco2002 <marcoclone@gmail.com>
2023-12-14 23:47:01 +0100

    add width and height control

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <v-app>
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Navigation drawer"></v-list-item>
-      </v-list>
+    <v-navigation-drawer :width="400">
+      
+      <div class="pa-8">
+        <h2 class="text-center pb-4">Configuration</h2>
+        <v-text-field class="inline-block w-1/2 pr-2" label="width" type="number" :rules="[validateWidthAndHeight]" variant="outlined" v-model="width"></v-text-field>
+        <v-text-field class="inline-block w-1/2 pl-2" label="height" type="number" :rules="[validateWidthAndHeight]" variant="outlined" v-model="height"></v-text-field>
+      </div>
+      
     </v-navigation-drawer>
     <v-main class="content-center grid">
-      <Chessboard class="justify-self-center" :width="8" :height="8" />
+      <Chessboard class="justify-self-center" :width="Math.min(parseInt(width), 8)" :height="Math.min(parseInt(height), 8)" />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import {ref} from 'vue'
 import Chessboard from '@/components/Chessboard.vue'
+let width = ref("8"), height = ref("8")
+
+function validateWidthAndHeight(value) {
+  return value > 0 && value <= 8;
+}
 </script>
